b9977d13116d1191b06256d83eab95216a2ce884
avinash <avinashvmetre20@gmail.com>
2026-04-24 11:59:02 +0000

    start file added

--- file1.sh
+++ file1.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "logged in successfully"
-
+read -sp "Enter Password: " password
+echo
+if [ "$password" == "1234" ]; then
+	echo "Access Granted"
+	./file2.sh
+else
+	echo "password wrong"
+	exit 1
+fi
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "file running  started"
+./file1.sh
